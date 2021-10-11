4011800d028f617aec27ac515f5d5ab295e1ff8a
Mazantii <joakimmazanti@gmail.com>
2021-10-12 00:14:45 +0200

    Fungerende skud tilføjet

---
+++ ZooGoneWild/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public Rigidbody Bullet;
+    public Transform FireTransform;
+    public AudioSource ShootingAudio;
+    public AudioClip FireClip;
+    public float BulletSpeed;
+    public float BulletCooldown;
+
+    private bool Fired;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+
+        if (BulletCooldown > 0)
+        {
+            BulletCooldown -= 1 * Time.deltaTime;
+        }
+
+    }
+    void OnFire(InputValue FireValue)
+    {
+        if (BulletCooldown <= 0)
+        {
+            Fire();
+            BulletCooldown += 1;
+        }
+    }
+
+    private void Fire()
+    {
+        Rigidbody BulletInstance = Instantiate(Bullet, FireTransform.position, FireTransform.rotation) as Rigidbody;
+
+        BulletInstance.velocity = BulletSpeed * FireTransform.forward;
+    }
+
+}
