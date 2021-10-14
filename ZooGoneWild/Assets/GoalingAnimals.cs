c4d5a1b38af3871387ffa99dfd5e5e360fe224ee
Stine Galsgaard <stine@galsgaard.com>
2021-10-14 15:23:13 +0100

    pickups
    can goal, has picked up

--- ZooGoneWild/Assets/GoalingAnimals.cs
+++ ZooGoneWild/Assets/GoalingAnimals.cs
@@ -6,34 +6,40 @@ public class GoalingAnimals : MonoBehaviour
 {
     public Transform Goal;            //goal
     public GameObject Animal;         //animal to keep
-    public GameObject ThisPlayer; 
+    public GameObject ThisPlayer;     //den player hvis goalzone det er
 
-    private bool CanGoal = false;     
+    public TouchPickUp touchpickup;
+    public bool CanGoal = false;
 
     void Update()
     {
         if (CanGoal) //hvis det er true at man er within the collider af et animal tag object
         {
-           Animal.transform.parent = null; //disabler animal
-           Animal.GetComponent<BoxCollider>().enabled = false;
+           Animal.transform.parent = null;                          //sætter animals parent til goal
+           Animal.GetComponent<BoxCollider>().enabled = false;      //disabler animals collider
+           Animal.GetComponent<CapsuleCollider>().enabled = false;  //disabler animals trigger collider
         }
     }
 
-    void OnTriggerEnter(Collider other) 
-    {
-       
-        if (other.tag == "Animal" && ThisPlayer.GetComponent<CapsuleCollider>()) //tjekker hvis det 'tag' på den collider man går ind i er et animal
+    void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject == ThisPlayer) //tjekker hvis det object der collider er goalzonens player
         {
+            if (touchpickup.HasPickedUp == true)
+            {
             CanGoal = true; //man aflevere
+            }
         }
+     
     }
 
-    void OnTriggerExit(Collider other) 
+    void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Animal" && ThisPlayer.GetComponent<CapsuleCollider>()) //tjekker hvis det 'tag' på den collider der rører en er et animal
+        if (other.gameObject == ThisPlayer) //tjekker hvis det object på den collider er den player hvis goalzone det er
         {
             CanGoal = false; //man kan ikke aflevere
         }
     }
+    
 
 }
--- ZooGoneWild/Assets/Scripts/TouchPickUp.cs
+++ ZooGoneWild/Assets/Scripts/TouchPickUp.cs
@@ -6,12 +6,15 @@ public class TouchPickUp : MonoBehaviour
 {
     public GameObject Animal;
     public Transform Player;
+    
+    public bool HasPickedUp;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Animal")
         {
             Animal.transform.parent = Player.transform;
+            HasPickedUp = true;
         }
     }
 }
